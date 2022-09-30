b7ee864404fd6253681d642b2e37fa52c89e2620
Jonas Ohland <jonas.ohland@gmail.com>
2022-09-30 20:09:17 +0200

    buffers - generic impl for message stream peer address

--- crates/rist-rs-buffers/src/stream/message/mod.rs
+++ crates/rist-rs-buffers/src/stream/message/mod.rs
@@ -5,6 +5,8 @@ mod non_blocking;
 
 pub trait MessageStreamPeerAddress: Clone + Copy + Hash + Debug + Eq {}
 
+impl<T> MessageStreamPeerAddress for T where T: Clone + Copy + Hash + Debug + Eq {}
+
 pub use non_blocking::NonBlockingMessageStream;
 pub use non_blocking::NonBlockingMessageStreamAcceptor;
 pub use non_blocking::NonBlockingMessageStreamError;
